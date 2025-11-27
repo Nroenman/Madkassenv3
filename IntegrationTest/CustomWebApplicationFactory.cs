f1297aeb98dc6d9477051b4f17b4a2b750327799
KaderKivrak <119049854+KaderKivrak@users.noreply.github.com>
2025-11-27 17:37:00 +0100

    commit

---
+++ IntegrationTest/CustomWebApplicationFactory.cs
@@ -0,0 +1,30 @@
+﻿using MadkassenRestAPI.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IntegrationTest
+{
+    public class CustomWebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor =
+                    services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TestDatabase");
+                });
+            });
+        }
+    }
+}
