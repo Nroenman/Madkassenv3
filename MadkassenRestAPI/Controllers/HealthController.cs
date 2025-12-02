a6e5528b3338df8fa162f581af4562b79b873bf2
Nikolai Rønman <nikolairoenman@gmail.com>
2025-12-02 20:58:18 +0100

    1
    1

---
+++ MadkassenRestAPI/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MadkassenRestAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("OK");
+    }
+}
