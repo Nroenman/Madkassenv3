53c7e24884ac59d0e60c7597d49ca653fe544233
DehliFin <dehli1212@gmail.com>
2025-12-03 13:16:54 +0100

    more test

--- MadkassenTest/Whitebox/ProductTest.cs
+++ MadkassenTest/Whitebox/ProductTest.cs
@@ -1,4 +1,5 @@
-﻿using ClassLibrary.Model;
+﻿using ClassLibrary;
+using ClassLibrary.Model;
 using MadkassenRestAPI.Models;
 using MadkassenRestAPI.Services;
 using Microsoft.AspNetCore.Cors.Infrastructure;
@@ -17,24 +18,18 @@ namespace MadkassenTest.Whitebox
         private readonly ApplicationDbContext context;
         public ProductTest()
         {
-            var connection = new SqliteConnection("DataSource=:memory:");
-            connection.Open();
-
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseSqlite(connection)
-                .Options;
+                    .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                    .Options;
 
             context = new ApplicationDbContext(options);
         }
 
-
-
         [Fact]
         public async Task GetproductbyidWhereIdExistAsync()
         {
-            
-            context.Database.EnsureCreated();   // opretter tabellerne
 
+            context.Database.EnsureCreated();   // opretter tabellerne
 
             ProductService service = new ProductService(context);
 
@@ -60,7 +55,6 @@ namespace MadkassenTest.Whitebox
 
             context.Database.EnsureCreated();   // opretter tabellerne
 
-
             ProductService service = new ProductService(context);
 
             var result = service.GetProductByIdAsync(99999);
@@ -69,12 +63,147 @@ namespace MadkassenTest.Whitebox
         }
 
         [Fact]
-        public void AddProductAsync()
+        public async Task AddProductAsync()
         {
+            context.Database.EnsureCreated();
 
+            ProductService service = new ProductService(context);
+
+            var product = new Produkter
+            {
+                ProductId = 1,
+                ProductName = "Test product",
+                Price = 100,
+                StockLevel = 10,
+                CategoryId = 1
+            };
+
+            var result = await service.AddProductAsync(product);
+            Assert.IsType<Produkter>(result);
         }
 
+        [Fact]
+        public async Task AddProductAlredyExistAsync()
+        {
+            context.Database.EnsureCreated();
 
+            ProductService service = new ProductService(context);
 
+            var product = new Produkter
+            {
+                ProductId = 1,
+                ProductName = "Test product",
+                Price = 100,
+                StockLevel = 10,
+                CategoryId = 1
+            };
+
+            context.Produkter.Add(product);
+            await context.SaveChangesAsync();
+
+            await Assert.ThrowsAsync<InvalidOperationException>(
+       () => service.AddProductAsync(product));
+        }
+
+        [Fact]
+        public async Task AddProductWithNullAsync()
+        {
+            context.Database.EnsureCreated();
+
+            ProductService service = new ProductService(context);
+
+            Produkter product = null;
+
+            await Assert.ThrowsAsync<ArgumentNullException>(
+       () => service.AddProductAsync(product));
+        }
+
+        [Fact]
+        public async void UpdateProductStockAsync()
+        {
+            context.Database.EnsureCreated();
+
+            ProductService service = new ProductService(context);
+
+            var product = new Produkter
+            {
+                ProductId = 1,
+                ProductName = "Test product",
+                Price = 100,
+                StockLevel = 1,
+                CategoryId = 1
+            };
+
+            context.Produkter.Add(product);
+            await context.SaveChangesAsync();
+
+            var result = await service.UpdateProductStockAsync(1,10);
+            Assert.True(result.StockLevel == 11);
+        }
+        [Fact]
+        public async void UpdateProductStockwith0()
+        {
+            context.Database.EnsureCreated();
+
+            ProductService service = new ProductService(context);
+
+            var product = new Produkter
+            {
+                ProductId = 1,
+                ProductName = "Test product",
+                Price = 100,
+                StockLevel = 0,
+                CategoryId = 1
+            };
+
+            context.Produkter.Add(product);
+            await context.SaveChangesAsync();
+
+            var result = await service.UpdateProductStockAsync(1, -1);
+            Assert.Null(result);
+        }
+        [Fact]
+        public async void UpdateProductStockwithnull()
+        {
+            context.Database.EnsureCreated();
+
+            ProductService service = new ProductService(context);
+
+            Produkter product = null;
+
+            var result = await service.UpdateProductStockAsync(1, 1);
+            Assert.Null(result);
+        }
+        [Fact]
+        public async void GetProductsByCategoryAsync()
+        {
+            context.Database.EnsureCreated();
+
+            var service = new ProductService(context);
+
+            var product = new Produkter
+            {
+                ProductId = 1,
+                ProductName = "Test product",
+                Price = 100,
+                StockLevel = 0,
+                CategoryId = 1
+            };
+
+            var category = new Kategori
+            {
+                CategoryId = 1,
+                CategoryName = "Category",
+                Description = "Description",
+            };
+
+            context.Kategori.Add(category);
+
+            context.Produkter.Add(product);
+            await context.SaveChangesAsync();
+
+            var result = await service.GetProductsByCategoryAsync(1);
+            Assert.IsType<List<Produkter>>(result);
+        }
     }
 }
